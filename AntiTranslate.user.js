171c9185d7955f75641147256e8d1f2cb7a6bf14
Adriaan <adriaan1313@outlook.com>
2025-07-24 11:02:40 +0200

    bump version to 0.70

--- AntiTranslate.user.js
+++ AntiTranslate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Auto-translate Canceler
 // @namespace    https://github.com/adriaan1313/YoutubeAutotranslateCanceler
-// @version      0.69.4
+// @version      0.70.0
 // @description  Remove auto-translated youtube titles
 // @author       Pierre Couy
 // @match        https://www.youtube.com/*
